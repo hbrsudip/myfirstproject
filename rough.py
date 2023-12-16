0cd6d96ee76498e7037e2d4d3fb4e3c317ad0db8
hbrsudip <95857049+hbrsudip@users.noreply.github.com>
2023-12-16 17:10:13 +0530

    Update rough.py
    first changes

--- rough.py
+++ rough.py
@@ -1,23 +1,5 @@
-# Load the workbook
-path = 'test.xlsx'
-import xlwings as xw
+#first time working 
+print("Hello world")
 
-# Connect to an existing Excel instance or create a new one
-app = xw.App(visible=False)
-
-# Open the workbook
-wb = app.books.open(path)
-
-# Access the active sheet
-sheet = wb.sheets['Review Protocol Cover Sheet']
-sheet['F12'].value = "Hi"
-
-# Save changes and close the workbook
-try:
-    wb.save('C:/Users/z0050604/OneDrive - ZF Friedrichshafen AG/Apps/My tools/RCR Test Automation/rough/test3.xlsx')
-except Exception as e:
-    print(e)
-wb.close()
-
-# Quit Excel
-app.quit()
+a = "Hello world"
+print(a.split(' '))
