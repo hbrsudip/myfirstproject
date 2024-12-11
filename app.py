dea407e39111dc4529092325ff5f1c7290120e7a
Bachcha Ram Harijan <95857049+hbrsudip@users.noreply.github.com>
2024-12-11 10:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-#first time working 
-print("Hello world")
-a = "Hello world"
-print(a.split(' '))
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(message="Hello, Flask on Vercel!")
+
+if __name__ == "__main__":
+    app.run()
